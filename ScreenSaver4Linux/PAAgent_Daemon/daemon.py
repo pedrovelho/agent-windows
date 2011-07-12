9a5564ee6972adc2cb31d4c57a82e9ec50de7c25
pgouttef <pgouttef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-07-12 07:52:42 +0000

    Final version with documentation and README files.
    clean up folders.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/agent/windows/branches/SHORT_JavaConfEditor@20133 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ ScreenSaver4Linux/PAAgent_Daemon/daemon.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+#
+# ################################################################
+#
+# ProActive Parallel Suite(TM): The Java(TM) library for
+#    Parallel, Distributed, Multi-Core Computing for
+#    Enterprise Grids & Clouds
+#
+# Copyright (C) 1997-2011 INRIA/University of
+#                 Nice-Sophia Antipolis/ActiveEon
+# Contact: proactive@ow2.org or contact@activeeon.com
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 of
+# the License.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+# USA
+#
+# If needed, contact us to obtain a release under GPL Version 2 or 3
+# or a different license than the AGPL.
+#
+#  Initial developer(s):               The ActiveEon Team
+#                        http://www.activeeon.com/
+#  Contributor(s):
+#
+# ################################################################ 
+# $$ACTIVEEON_CONTRIBUTOR$$
+#
+
+##################################
+#	 		DAEMON		         #
+##################################
+
+
+import sys, os, time, atexit
+from signal import SIGTERM
+
+import commands
+
+from model import Model
+
+# The daemon witch turn into process list on Linux.
+# This daemon is ab
+class Daemon:
+        
+        def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+                self.stdin = stdin
+                self.stdout = stdout
+                self.stderr = stderr
+                self.pidfile = pidfile
+       
+        def daemonize(self):
+                """
+                do the UNIX double-fork magic, see Stevens' "Advanced
+                Programming in the UNIX Environment" for details (ISBN 0201563177)
+                http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+                """
+                try:
+                        pid = os.fork()
+                        if pid > 0:
+                                # exit first parent
+                                sys.exit(0)
+                except OSError, e:
+                        sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+                        sys.exit(1)
+       
+                # decouple from parent environment
+                os.chdir("/")
+                os.setsid()
+                os.umask(0)
+       
+                # do second fork
+                try:
+                        pid = os.fork()
+                        if pid > 0:
+                                # exit from second parent
+                                sys.exit(0)
+                except OSError, e:
+                        sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+                        sys.exit(1)
+       
+                # redirect standard file descriptors
+                sys.stdout.flush()
+                sys.stderr.flush()
+                si = file(self.stdin, 'r')
+                so = file(self.stdout, 'a+')
+                se = file(self.stderr, 'a+', 0)
+                os.dup2(si.fileno(), sys.stdin.fileno())
+                os.dup2(so.fileno(), sys.stdout.fileno())
+                os.dup2(se.fileno(), sys.stderr.fileno())
+       
+                # write pidfile
+                atexit.register(self.delpid)
+                pid = str(os.getpid())
+                file(self.pidfile,'w+').write("%s\n" % pid)
+       
+        def delpid(self):
+                os.remove(self.pidfile)
+ 
+        def start(self):
+                """
+                Start the daemon
+                """
+                print('start')
+                # Check for a pidfile to see if the daemon already runs
+                try:
+                        pf = file(self.pidfile,'r')
+                        pid = int(pf.read().strip())
+                        pf.close()
+                except IOError:
+                        pid = None
+       
+                if pid:
+                        message = "pidfile %s already exist. Daemon already running?\n"
+                        sys.stderr.write(message % self.pidfile)
+                        sys.exit(1)
+               
+                # Start the daemon
+                self.daemonize()
+                self.run()
+ 
+        def stop(self):
+                """
+                Stop the daemon
+                """
+                # Get the pid from the pidfile
+                print('stop')
+                try:
+                        pf = file(self.pidfile,'r')
+                        pid = int(pf.read().strip())
+                        pf.close()
+                except IOError:
+                        pid = None
+       
+                if not pid:
+                        message = "pidfile %s does not exist. Daemon not running?\n"
+                        sys.stderr.write(message % self.pidfile)
+                        return # not an error in a restart
+ 
+                # Try killing the daemon process       
+                try:
+                        while 1:
+                                os.kill(pid, SIGTERM)
+                                time.sleep(0.1)
+                except OSError, err:
+                        err = str(err)
+                        if err.find("No such process") > 0:
+                                if os.path.exists(self.pidfile):
+                                        os.remove(self.pidfile)
+                        else:
+                                print str(err)
+                                sys.exit(1)
+ 
+        def restart(self):
+                """
+                Restart the daemon
+                """
+                self.stop()
+                self.start()
+  
+        def launchScreenSaver(self):
+            Model().launcher('ScreenSaver')
+                
+        def startJVM(self):
+            Model().launcher('startJVM')
+            
+        def stopJVM(self):
+            Model().launcher('stopJVM')
+ 
+        def run(self):
+                """
+                You should override this method when you subclass Daemon. It will be called after the process has been
+                daemonized by start() or restart().
+                """
+                
---
+++ ScreenSaver4Linux/PAAgent_Daemon/login.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+#
+# ################################################################
+#
+# ProActive Parallel Suite(TM): The Java(TM) library for
+#    Parallel, Distributed, Multi-Core Computing for
+#    Enterprise Grids & Clouds
+#
+# Copyright (C) 1997-2011 INRIA/University of
+#                 Nice-Sophia Antipolis/ActiveEon
+# Contact: proactive@ow2.org or contact@activeeon.com
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 of
+# the License.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+# USA
+#
+# If needed, contact us to obtain a release under GPL Version 2 or 3
+# or a different license than the AGPL.
+#
+#  Initial developer(s):               The ActiveEon Team
+#                        http://www.activeeon.com/
+#  Contributor(s):
+#
+# ################################################################ 
+# $$ACTIVEEON_CONTRIBUTOR$$
+#
+
+import commands
+import string
+import ConfigParser
+
+# A class Login to check differents access of the Daemon using ConfigParser standard API of Python
+class Login:
+    
+    def checkAccess(self,username): 
+        config = ConfigParser.RawConfigParser()
+        
+        config.read( '/usr/bin/PAAgent/config.txt' )
+        items = config.items('USERS-ACCESS')
+        
+        for user in items: 
+            # user[NAME,RIGHTS]
+            if user[1] == 'yes' and user[0] == username:
+                return True
+            
+        return False
