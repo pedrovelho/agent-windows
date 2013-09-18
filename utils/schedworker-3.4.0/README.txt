7fdd7c2ab18ce67e6b9a75b4b980cbafd4996c5c
Vladimir Bodnartchouk <Vladimir.Bodnartchouk@activeeon.com>
2013-09-18 15:34:40 +0200

    AGENT-210 - Add schedwork-3.4.0 for standalone
    Change-Id: Ie247c8471d13bfb359d589f25a567c79a8241123

---
+++ utils/schedworker-3.4.0/README.txt
@@ -0,0 +1,18 @@
+Thanks for your interest in ProActive Scheduling.
+
+ProActive Scheduling 3.4.0 (worker version)
+
+You can find the documentation of ProActive Scheduler in the docs directory.
+
+Javadoc and updated documentation are available online: http://proactive.inria.fr
+
+If you have any problems or questions when using ProActive Scheduling,
+feel free to contact us at proactive@ow2.org
+
+
+*** Known bugs and issues:
+
+Details can be found on the ProActive Jira bug-tracking system
+(http://bugs.activeeon.com/):
+
+*** Enjoy ProActive Scheduling !
---
+++ utils/schedworker-3.4.0/bin/windows/init.bat
@@ -0,0 +1,89 @@
+@echo off
+rem ----------------------------------------------------------------------------
+rem
+rem This variable should be set to the directory where is installed ProActive
+rem
+
+IF NOT DEFINED PA_SCHEDULER set PA_SCHEDULER=%CD%\..\..
+
+for %%f in ("%PA_SCHEDULER%") do set PA_SCHEDULER=%%~sf
+
+rem ----------------------------------------------------------------------------
+
+
+if NOT DEFINED JAVA_HOME goto javahome
+if "%JAVA_HOME%" == "" goto javahome
+
+rem ----
+rem Set up the classpath using classes dir or jar files
+rem ----
+
+IF DEFINED CLASSPATHEXT (
+	SET CLASSPATH=%CLASSPATHEXT%
+) ELSE (
+	SET CLASSPATH=.
+)
+
+rem Script engines must be added to classpath to be found
+rem it must also placed before jars containing jar-index
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\jruby.jar
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\sigar\sigar.jar
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\jython-2.5.4-rc1.jar
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\groovy-all-2.1.5.jar
+rem  Needed explicitly by VFS (file transfer in pre/post script
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\commons-logging-1.1.1.jar
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\commons-httpclient-3.1.jar
+rem fill with ProActive.jar : use jar index for proActive dependencies
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\ProActive.jar
+rem fill with Scheduler jars : use jar index for Scheduler dependencies
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\ProActive_SRM-common.jar
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\ProActive_ResourceManager.jar
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\ProActive_Scheduler-core.jar
+SET JARS=!JARS!;%PA_SCHEDULER%\dist\lib\ProActive_Scheduler-mapreduce.jar
+FOR %%j IN ("%PA_SCHEDULER%\addons\*.jar") DO SET JARS=!JARS!;%%j
+
+SET CLASSPATH=%CLASSPATH%;%JARS%;%PA_SCHEDULER%\addons
+
+rem log4j file
+IF "%1"=="" (
+	set LOG4J_FILE=file:/%PA_SCHEDULER:\=/%/config/log4j/log4j-client
+) ELSE (
+	set LOG4J_FILE=file:/%PA_SCHEDULER:\=/%/config/log4j/%1
+)
+
+rem if log4j file is server, use server security manager
+rem otherwise, use standard ProActive java.security.policy file
+IF "%1"=="scheduler-log4j-server" (
+	GOTO sjps
+)
+IF "%1"=="rm-log4j-server" (
+	:sjps
+	set JAVA_POLICY=file:%PA_SCHEDULER%/config/security.java.policy-server
+	GOTO sjps_
+) ELSE (
+	set JAVA_POLICY=file:%PA_SCHEDULER%/config/security.java.policy-client
+)
+:sjps_
+
+set JAVA_CMD="%JAVA_HOME%\bin\java.exe" -Dproactive.home="%PA_SCHEDULER%" -Dproactive.configuration="%PA_SCHEDULER%\config\proactive\ProActiveConfiguration.xml" -Dpa.scheduler.home="%PA_SCHEDULER%" -Dpa.rm.home="%PA_SCHEDULER%" -Djava.security.manager -Djava.security.policy="%JAVA_POLICY%" -Dlog4j.configuration="%LOG4J_FILE%"
+
+rem Adding java tools to the path
+SET OK=1
+FOR /F "delims=;" %%i IN ("%PATH%") DO (
+IF /I "%%i" == "%JAVA_HOME%\bin" SET OK=0
+)
+IF /I %OK%==1 SET PATH=%JAVA_HOME%\bin;%PATH%
+
+goto end
+
+
+:javahome
+echo.
+echo The environment variable JAVA_HOME must be set the current jdk distribution
+echo installed on your computer. 
+echo Use 
+echo    set JAVA_HOME=<the directory where is the JDK>
+goto end
+
+ 
+:end
