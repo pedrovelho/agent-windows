1ba5dbd7b796e33643aac328fb2d431869644ec8
Vladimir Bodnartchouk <vladimir.bodnartchouk@activeeon.com>
2014-08-13 14:00:07 +0200

    added a js script to dumps NSIS directives to install_schedowrker.nsi

---
+++ utils/dump_install_schedworker.js
@@ -0,0 +1,40 @@
+importPackage(java.lang);
+importPackage(java.io);
+importPackage(java.util);
+importPackage(java.util.concurrent);
+importPackage(java.util.zip);
+
+var installerFile = new File("install_schedworker.nsi");
+var prefix = new File(System.getProperty("user.dir")).getName();
+
+if (!installerFile.exists()) {   
+   installerFile.createNewFile();
+}
+
+var out = new PrintWriter(installerFile);
+
+// dump all files with relative paths to 
+var schedworkerDir = new File("schedworker");
+printPath(schedworkerDir)
+
+out.close();
+
+function printPath(file){
+	if (file.isDirectory()) {
+		out.println("SetOutPath $INSTDIR" + File.separator + file.getPath())
+		var files = file.listFiles(new FileFilter({
+    		accept: function (fff) {
+    			return fff.isFile();
+		}}));
+		for (i in files) {
+			out.println("File \""+ prefix + File.separator + files[i].getPath() + "\"");
+		}
+		var dirs = file.listFiles(new FileFilter({
+    		accept: function (fff) {
+    			return fff.isDirectory();
+		}}));
+		for (i in dirs) {
+			printPath(dirs[i]);
+		}
+    }   
+}
