1d0b88a444393ae305fbd554dfaa1cce3861cd55
Vladimir Bodnartchouk <vladimir.bodnartchouk@activeeon.com>
2014-08-18 17:01:42 +0200

    Remove schedworker dir contents that must now  be filled dynamically with the contents of the ProActiveNode.zip (see the jenkins build).

--- utils/dump_install_schedworker.js
+++ utils/dump_install_schedworker.js
@@ -4,37 +4,46 @@ importPackage(java.util);
 importPackage(java.util.concurrent);
 importPackage(java.util.zip);
 
+var excludes = ['.so','.sl','.sh', '.dylib'];
 var installerFile = new File("install_schedworker.nsi");
 var prefix = new File(System.getProperty("user.dir")).getName();
 
-if (!installerFile.exists()) {   
-   installerFile.createNewFile();
+if (!installerFile.exists()) {
+	installerFile.createNewFile();
 }
 
 var out = new PrintWriter(installerFile);
-
 // dump all files with relative paths to 
 var schedworkerDir = new File("schedworker");
 printPath(schedworkerDir)
-
 out.close();
 
 function printPath(file){
 	if (file.isDirectory()) {
 		out.println("SetOutPath $INSTDIR" + File.separator + file.getPath())
 		var files = file.listFiles(new FileFilter({
-    		accept: function (fff) {
-    			return fff.isFile();
+			accept: function (fff) {
+				return fff.isFile();
 		}}));
 		for (i in files) {
-			out.println("File \""+ prefix + File.separator + files[i].getPath() + "\"");
+			var filename = files[i].getName();
+			var exclude = false;
+			for (e in excludes) {
+				if (filename.endsWith(excludes[e])){
+					exclude = true;
+					break;
+				}
+			}
+			if(!exclude){
+				out.println("File \""+ prefix + File.separator + files[i].getPath() + "\"");
+			}
 		}
 		var dirs = file.listFiles(new FileFilter({
-    		accept: function (fff) {
-    			return fff.isDirectory();
+			accept: function (fff) {
+				return fff.isDirectory();
 		}}));
 		for (i in dirs) {
 			printPath(dirs[i]);
 		}
-    }   
+	}
 }
