23d2d9b3ebf671fe8640d73e54e90499eba8a09c
Sergei Dolgov <Sergei.Dolgov@activeeon.com>
2014-12-15 11:51:20 +0100

    Bump version to 6.1.0

--- AgentForAgent/AgentWindow.Designer.cs
+++ AgentForAgent/AgentWindow.Designer.cs
@@ -234,7 +234,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "ProActiveAgent v6.0.0";
+            this.label3.Text = "ProActiveAgent v6.1.0";
             // 
             // label4
             // 
--- installer_script.nsi
+++ installer_script.nsi
@@ -37,7 +37,7 @@
      !define FILENAME_SUFIX ""
 !endif
 
-!define VERSION "6.0.0"
+!define VERSION "6.1.0"
 !define PRODUCT_NAME "ProActive Agent"
 !define PRODUCT_WEB_SITE "http://doc.activeeon.com"
 !define SERVICE_NAME "ProActiveAgent"
