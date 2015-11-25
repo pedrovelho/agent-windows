6c2016a6c605693f07774e22be14ad08d7c3c0b7
Laurent Pellegrino <laurent.pellegrino@activeeon.com>
2015-11-25 14:07:26 +0100

    Remove hard-coded version number

--- AgentForAgent/AgentWindow.Designer.cs
+++ AgentForAgent/AgentWindow.Designer.cs
@@ -234,7 +234,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "ProActiveAgent v6.1.0";
+            this.label3.Text = "ProActive Agent";
             // 
             // label4
             // 
@@ -244,7 +244,7 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(136, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "(C) 2015 INRIA/ActiveEon ";
+            this.label4.Text = "(C) 2015 INRIA/ActiveEon";
             // 
             // timer
             // 
